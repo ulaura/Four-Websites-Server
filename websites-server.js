a229b7ac091d62b97bb860f9c6e96a2f355397f6
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-07 15:51:10 -0700

    Fixed the logic for the server.

--- websites-server.js
+++ websites-server.js
@@ -13,6 +13,74 @@ server.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
 
+//var thisPage = "";
 
 function handleRequest(req, res) {
+  // Capture the url the request is made to
+  var path = req.url;
+
+  // Depending on the URL, display a different HTML file.
+  switch (path) {
+
+    case "/":
+      return displayRoot(req, res);
+
+    case "/favoritefood":
+      return displayFavoriteFood(req, res);
+
+    case "/favoritemovies":
+    	return displayFavoriteMovies(req, res);
+
+    case "/favoritecss":
+    	return displayFavoriteCSS(req, res);
+
+    default:
+      return display404(req, res);
+  }
+}
+
+
+
+function displayRoot(req, res) {
+  	fs.readFile(__dirname + "/index.html", function(err, data) {
+    	res.writeHead(200, { "Content-Type": "text/html" });
+    	res.end(data);
+  	});
+
+};
+
+function displayFavoriteFood (req, res) {
+  	fs.readFile(__dirname + "/favoritefood.html", function(err, data) {
+    	res.writeHead(200, { "Content-Type": "text/html" });
+    	res.end(data);
+  	});
+
+};
+
+
+function displayFavoriteMovies (req, res) {
+  	fs.readFile(__dirname + "/favoritemovies.html", function(err, data) {
+    	res.writeHead(200, { "Content-Type": "text/html" });
+    	res.end(data);
+  	});
+
+};
+
+function displayFavoriteCSS (req, res) {
+  	fs.readFile(__dirname + "/favoritecss.html", function(err, data) {
+    	res.writeHead(200, { "Content-Type": "text/html" });
+    	res.end(data);
+  	});
+
+};
+
+function display404 (req, res) {
+  var myHTML =  "<html>" +
+    "<body><h1>404 Not Found </h1>" +
+    "<p>The page you were looking for can not be found</p>" +
+    "</body></html>";
+
+  res.writeHead(404, { "Content-Type": "text/html" });
+  res.end(myHTML);
+};
 
